0f35eb0267c056a3983123a837ba07d033b2ce1a
Pranish Shrestha <pranish.shres@gmail.com>
2016-11-04 17:20:15 +0545

    daily progress committed

--- app/src/main/java/com/example/pranishres/myapplication/MainActivity.java
+++ app/src/main/java/com/example/pranishres/myapplication/MainActivity.java
@@ -4,13 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     String MY_TAG = "custom message";
 
+    private EditText password ;
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,8 +23,21 @@ public class MainActivity extends AppCompatActivity {
         // The Simple Hello World Activity
 //        setContentView(R.layout.activity_main);
 
+
         // Activity which takes two numbers from user and adds it
-        setContentView(R.layout.add_two_numbers);
+//        setContentView(R.layout.basic_calculator);
+
+
+        // Using password field and show password to the user
+ /*       setContentView(R.layout.show_password);
+        // calling this method will register a button click event
+        addListenerOnButton();
+*/
+
+        // Check box basics
+            setContentView(R.layout.checkbox_and_radio);
+        // calling this method will register a button click event
+            addListenerForCheckbox();
 
         // Printing Logs
         Log.i(MY_TAG, "onCreate()");
@@ -62,8 +80,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * Layout name :- add_two_numbers
-     * Method Type :- Handle button click event
+     * Layout name :- basic_calculator
+     * Method Type :- Button click event
      * Purpose     :- Take the values for 2 numbers provided by the user and display the sum result.
      * @param v
      */
@@ -86,6 +104,60 @@ public class MainActivity extends AppCompatActivity {
         Log.i(MY_TAG, "The sum is : " + (number1+number2));
 
         t1.setText(Integer.toString(number1+number2));
+
+    }
+
+    /**
+     * Layout name :- show_password
+     * Method Type :- Call
+     * Purpose     :- Handles button click event and displays the password entered by the user.
+     */
+    public void addListenerOnButton(){
+        password = (EditText) findViewById(R.id.editText_Password_PwddField);
+        button = (Button) findViewById(R.id.btn_ShowPassword);
+
+        // Add button click event
+        button.setOnClickListener(
+                new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v){
+                        Toast.makeText(
+                                MainActivity.this , password.getText() , Toast.LENGTH_SHORT
+                        ).show();
+                    }
+                }
+
+        );
+    }
+
+    /**
+     * Layout name  :- checkbox_and_radio
+     * Method Type  :- Call
+     * Purpose      :- Displays the checked items
+     */
+    public void addListenerForCheckbox(){
+        final CheckBox checkboxSwimming = (CheckBox) findViewById(R.id.checkBox_swimming);
+        final CheckBox checkboxDancing  = (CheckBox) findViewById(R.id.checkBox_dancing);
+        final CheckBox checkboxSinging  = (CheckBox) findViewById(R.id.checkBox_singing);
+        final Button button_check = (Button)   findViewById(R.id.button_submit);
+
+        // Add button click event
+        button_check.setOnClickListener(
+                new View.OnClickListener(){
+                   @Override
+                    public void onClick(View v){
+
+                        // Append all the checkboxes in a String Buffer
+                        StringBuffer result = new StringBuffer();
+                        result.append("Swimming : ").append(checkboxSwimming.isChecked());
+                        result.append(" Dancing : ").append(checkboxDancing.isChecked());
+                        result.append(" Singing : ").append(checkboxSinging.isChecked());
+
+                        Toast.makeText(
+                                MainActivity.this , result.toString() , Toast.LENGTH_LONG
+                        ).show();
+                    }
+                });
 
     }
 }
