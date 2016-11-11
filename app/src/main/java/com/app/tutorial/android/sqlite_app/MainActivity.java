74a3a8f79f7b9cd70b0b94b98119d4943af7c3b3
Pranish Shrestha <pranish.shres@gmail.com>
2016-11-11 16:42:02 +0545

    Implemented basic ArrayAdapter(). Passed a string array to array adapter to populate listview

--- app/src/main/java/com/app/tutorial/android/sqlite_app/MainActivity.java
+++ app/src/main/java/com/app/tutorial/android/sqlite_app/MainActivity.java
@@ -1,101 +1,81 @@
 package com.app.tutorial.android.sqlite_app;
 
 import android.app.AlertDialog;
+import android.content.Intent;
 import android.database.Cursor;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.app.tutorial.android.sqlite_app.config.DatabaseConfig;
 import com.app.tutorial.android.sqlite_app.model.Student;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ActionBarActivity {
     DatabaseConfig dbConfig;
 
-    // Alternative to findViewById. Needs Butterknife dependencies
-    @BindView(R.id.editText_main_firstName)
-    EditText firstName;
-    @BindView(R.id.editText_main_lastName)
-    EditText lastName;
-    @BindView(R.id.editText_main_Address)
-    EditText address;
-    @BindView(R.id.editText_main_email)
-    EditText email;
-    @BindView(R.id.editText_main_getById)
-    EditText getById;
+
+
+    /*For adapter view*/
+    String[] allData = new String[]{
+            "Nepal",
+            "China",
+            "Japan",
+            "U.S.A",
+            "U.K",
+            "Spain",
+            "Portugal",
+            "Satungal",
+            "Latungal",
+            "Hatungal",
+            "Patungal",
+            "Matungal",
+            "France",
+            "Dance",
+            "Brance",
+            "aaNepal",
+            "aaChina",
+            "aaJapan",
+            "aaU.S.A",
+            "aaU.K",
+            "aaSpain",
+            "aaPortugal",
+            "aaSatungal",
+            "aaLatungal",
+            "aaHatungal",
+            "aPatungal",
+            "aMatungal",
+            "aFrance",
+            "aDance",
+            "aBrance",
+    };
+
+    List<String> list = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_student_list);
         dbConfig = new DatabaseConfig(this);
+
+        ListView lvCountries = (ListView) findViewById(R.id.array_adapter_listView);
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1  , allData);
+        lvCountries.setAdapter(arrayAdapter);
 
         // Needs to be called to enable Butterknife annotations
         ButterKnife.bind(this);
 
     }
 
-    public void onButtonSubmit(View view) {
-        Student student = new Student();
-        student.setFirstName(firstName.getText().toString());
-        student.setLastName(lastName.getText().toString());
-        student.setAddress(address.getText().toString());
-        student.setEmail(email.getText().toString());
-
-        Boolean result = dbConfig.insertData(student);
-
-        if (result) {
-            System.out.println("Data has been inserted successfully");
-            Toast.makeText(MainActivity.this, "Data has been inserted successfully", Toast.LENGTH_SHORT);
-        } else {
-            System.out.println("Error while inserting data");
-            Toast.makeText(MainActivity.this, "Error while inserting data", Toast.LENGTH_SHORT);
-        }
-    }
-
-    public void getAllData(View view) {
-        Cursor cursor = dbConfig.getAllData();
-        if (cursor.getCount() == 0) {
-            showMessage("Error", "No data found");
-            return;
-        }
-
-        StringBuffer stringBuffer = new StringBuffer();
-        while (cursor.moveToNext()) {
-            stringBuffer.append("Id : " + cursor.getString(0) + "\n");
-            stringBuffer.append("First Name : " + cursor.getString(1) + "\n");
-            stringBuffer.append("Last Name : " + cursor.getString(2) + "\n");
-            stringBuffer.append("Address : " + cursor.getString(3) + "\n");
-            stringBuffer.append("Email : " + cursor.getString(4) + "\n");
-        }
-
-        showMessage("Data", stringBuffer.toString());
-
-    }
-
-    public void getById(View view) {
-        Cursor cursor = dbConfig.getById(Integer.parseInt(getById.getText().toString()));
-
-        if (cursor.getCount() == 0) {
-            showMessage("Error", "No data found");
-            return;
-        }
-        while (cursor.moveToNext()) {
-            firstName.setText(cursor.getString(1));
-            lastName.setText(cursor.getString(2));
-        }
-    }
-
-    private void showMessage(String title, String message) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setCancelable(true);
-        builder.setTitle(title);
-        builder.setMessage(message);
-        builder.show();
-    }
 }
---
+++ app/src/main/java/com/app/tutorial/android/sqlite_app/student_list.java
@@ -0,0 +1,13 @@
+package com.app.tutorial.android.sqlite_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class student_list extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_list);
+    }
+}
